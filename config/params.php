604ec967a3c98da93b6504b84eb081fbb9827d1e
Antoine Ricci <aricci95@gmail.com>
2016-10-19 22:48:24 +0200

    ignore

---
+++ config/params.php
@@ -0,0 +1,17 @@
+<?php
+// NE PAS MODIFIER
+DEFINE("_HOST", "localhost");
+DEFINE("_USER", "root");
+DEFINE("_PASS", "");
+DEFINE("_BASE_DE_DONNEES", "startup_simulator");
+
+// FACEBOOK
+DEFINE('FACEBOOK_APP_ID', '');
+DEFINE('FACEBOOK_APP_SECRET', '');
+
+DEFINE('ADMIN_MAIL', '');
+DEFINE('MAIL_SERVER', 0);
+DEFINE('DEBUG', 0);
+DEFINE('PROFILER', 0);
+DEFINE('LOG', 1);
+DEFINE('LOG_FILE', 'startup.log');
